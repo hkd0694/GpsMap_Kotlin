b70d1f9f8e187c76003f239b9c8c9f3b68e4a72c
hkd0694 <gksrudehd123@naver.com>
2019-11-06 16:09:28 +0900

    Location Update

--- app/src/main/java/com/example/gpsmap/MapsActivity.kt
+++ app/src/main/java/com/example/gpsmap/MapsActivity.kt
@@ -2,6 +2,10 @@ package com.example.gpsmap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -12,7 +16,13 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private val REQUEST_ACCESS_FINE_LOCATION = 1000
+
     private lateinit var mMap: GoogleMap
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: MyLocationCallBack
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +31,30 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        locationinit();
+
+    }
+
+    private fun permissionCheck(cancel:()-> Unit, ok:() ->Unit){
+
+    }
+
+    private fun locationinit(){
+
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
+        locationCallback = MyLocationCallBack()
+        //GPS 우선
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        // 업데이터 인터벌
+        // 위치 정보가 없을 때는 업데이트 안 함
+        // 상황에 따라 짧아질 수 있음, 정확하지 않음
+        // 다른 앱에서 짧은 인터벌로 위치 정보를 요청하면 짧아질 수 있음
+        locationRequest.interval = 10000
+        //정확함. 이것보다 짧은 업데이터는 하지 않음
+        locationRequest.fastestInterval = 5000
     }
 
     /**
@@ -40,4 +74,28 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+
+    override fun onResume() {
+        super.onResume()
+        addLocationListener()
+    }
+
+    private fun addLocationListener(){
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback, null)
+    }
+
+    inner class MyLocationCallBack : LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult?) {
+            super.onLocationResult(locationResult)
+
+            val location = locationResult?.lastLocation
+
+            location?.run {
+                val latLng = LatLng(latitude,longitude)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17f))
+            }
+        }
+    }
+
+
 }
